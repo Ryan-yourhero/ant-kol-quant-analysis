8af4ed7dcf0a6c5e28ed4eb4f9171f97e864513c
Ryan <1422571398@qq.com>
2026-09-16 15:11:26 +0800

    ui: 顶部导航"历史记录"改为"历史操作记录"

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="nav">
       <router-link to="/">今日数据</router-link>
-      <router-link to="/history">历史记录</router-link>
+      <router-link to="/history">历史操作记录</router-link>
       <router-link to="/funds">基金方向库</router-link>
     </div>
     <div class="container">
